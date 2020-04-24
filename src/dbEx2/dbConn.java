0d35d03b06d9334cd465636bf042c50aa8a92291
Danielgimp <danielgimpelman@gmail.com>
2020-04-24 14:54:50 +0300

    Added Doctors mySQL db Insert

--- src/dbEx2/dbConn.java
+++ src/dbEx2/dbConn.java
@@ -3,18 +3,20 @@ package dbEx2;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class dbConn {
  	
-	 		String url = "jdbc:mysql://127.0.0.1" + "?useSSL=false";        
-	 		String username = "root";
-	 		String password = "k7f9h5d6";        
-	 		String sql = "CREATE DATABASE IF NOT EXISTS DoctorQueue";
+	 		static String url = "jdbc:mysql://127.0.0.1" + "?useSSL=false";        
+	 		static String username = "root";
+	 		static String password = "k7f9h5d6";        
+	 		static String sql = "CREATE DATABASE IF NOT EXISTS DoctorQueue";
 
-	 		dbConn()
+	 		private static Connection dbConnect() throws SQLException
 	 		{
-	 			try (Connection conn = DriverManager.getConnection(url, username, password);
-	 					PreparedStatement stmt = conn.prepareStatement(sql))
+	 			Connection conn = DriverManager.getConnection(url, username, password);
+	 			try (PreparedStatement stmt = conn.prepareStatement(sql))
 	 			{
 	 			stmt.execute();
 	 			}
@@ -22,8 +24,52 @@ public class dbConn {
 	 			{	 				
 	 				e.printStackTrace();
 	 			}
+				return conn;
 	 		}
 	 		
+	 		public static void createDoctorTable(Doctor arr[])
+	 		{
+	 			String CREATE_TABLE_SQL="CREATE TABLE IF NOT EXISTS DoctorQueue.doctors ("
+	                    + "doctor_id INT NOT NULL,"
+	                    + "doctor_type VARCHAR(255) NOT NULL,"
+	                    + "doctor_name VARCHAR(255) NOT NULL,"
+	                    + "salary REAL NOT NULL,"
+	                    + "PRIMARY KEY (doctor_id))"; 			
+	 			
+ 		 		
+	 			try {				 
+	 				Connection conn = dbConnect();
+	 				Statement stmt = conn.createStatement();
+	 				stmt.executeUpdate(CREATE_TABLE_SQL);
+	 	 			}
+	 					catch (SQLException e)
+	 					{
+	 						e.printStackTrace();
+	 					}
+	 			
+	 		for(int i=0;i<arr.length;i++)
+	 		{
+	 			String querry="INSERT INTO doctors " + "VALUES(?,?,?,?)";
+	 			
+	 			try (
+	 	                Connection conn = dbConnect();
+	 	                PreparedStatement statement = conn.prepareStatement(querry);)
+	 				{        
+	 	           	 	statement.setLong(i+1, arr[i].doctor_id);
+	 	                statement.setString(i+2, arr[i].doctor_type);
+	 	                statement.setString(i+3, arr[i].doctor_name);
+	 	                statement.setDouble(i+4, arr[i].salary);
+	 	                statement.addBatch();     
+	 	        	}
+	 					catch (SQLException ex) 
+	 					{
+	 	        				System.out.println(ex.getMessage());
+	 					}
+	 		}
 	 		
 	 	
+	 		}
+
+		
+	 		
 }
--- src/dbEx2/main.java
+++ src/dbEx2/main.java
@@ -5,7 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		dbConn Conn=new dbConn();
-		
+		Doctor doc=new Doctor();
+		Doctor docArr[]=new Doctor[] {doc};		
+		dbConn.createDoctorTable(docArr);
 		
 		
 		
